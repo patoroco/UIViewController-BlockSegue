5fe9410544be5cb55ff34224b02df762fdb71cf8
David Benko <dbenko@anypresence.com>
2014-05-13 15:29:08 -0400

    Updated fix to fix configureSegue: as well

--- JMGBlockSegue/UIViewController+BlockSegue.m
+++ JMGBlockSegue/UIViewController+BlockSegue.m
@@ -71,14 +71,14 @@ void BlockSegue(void) {
 
 #pragma mark - Public interface
 -(void)configureSegue:(NSString *)identifier withBlock:(UIViewControllerSegueBlock)block {
-    NSMutableDictionary *dBlocks = self.jmg_dictionaryBlock ?: [self jmg_createDictionaryBlock];
-    [dBlocks setObject:block forKey:identifier];
+    if (block) {
+        NSMutableDictionary *dBlocks = self.jmg_dictionaryBlock ?: [self jmg_createDictionaryBlock];
+        [dBlocks setObject:block forKey:identifier];
+    }
 }
 
 -(void)performSegueWithIdentifier:(NSString *)identifier sender:(id)sender withBlock:(UIViewControllerSegueBlock)block {
-    if (block) {
-        [self configureSegue:identifier withBlock:block];
-    }
+    [self configureSegue:identifier withBlock:block];
     [self performSegueWithIdentifier:identifier sender:sender];
 }
 
